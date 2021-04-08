92e25c1b2b43d3e3060a39a631eecb9676a460d7
sina <ie_sinakiani1@yahoo.com>
2021-04-08 19:27:39 +0430

    s1

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.notes.models.Note;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="my" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        Note note = new Note ( "title","content","time" );
+        Log.d ( TAG , "onCreate:myNote: " + note.toString () );
+
     }
 }
